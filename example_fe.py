1d7bd8e10fefd26796e5206dfe38ee2c3eb36e75
GuilhermeHaetinger <ghaetinger@gmail.com>
2019-06-14 14:52:24 -0300

    initialized website properly

--- example_fe.py
+++ example_fe.py
@@ -10,7 +10,24 @@ example_be.initialize()
 @app.route("/", methods=('GET', 'POST'))
 def home():
     abc = example_be.get_event_goers()
-    return render_template("home.html", abc=abc)
+    return render_template("home.html")
+
+@app.route("/candidatos", methods=('GET', 'POST'))
+def candidatos():
+    candidaturas = ['2016', '2010']
+    return render_template("candidato.html", candidaturas=candidaturas)
+
+@app.route("/candidatura/<candidatura>")
+def candidatura(candidatura):
+    return candidatura;
+
+@app.route("/mandatos", methods=('GET', 'POST'))
+def mandatos():
+    return render_template("mandato.html") 
+
+@app.route("/partidos", methods=('GET', 'POST'))
+def partidos():
+    return render_template("partido.html")
 
 if name == "__main__":
     app.run(debug=True)
